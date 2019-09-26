f68878de60f79743ecf4b55ccfc79b96e2728c85
Joakim Nikolai Schrøder <joakim@getacademy.no>
2019-09-26 13:46:54 +0200

    x

--- JavascriptUke2/avanserBildeKarusell.js
+++ JavascriptUke2/avanserBildeKarusell.js
@@ -36,9 +36,9 @@ carouselSlide.addEventListener('transitionend', ()=>{
         counter = carouselImages.length - 2;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
-    if (carouselImages[counter].id === 'firstClone') {
+    if (carouselImages[counter].id === 'firstClone'){
         carouselSlide.style.transition = "none";
-        counter = carouselImages - counter;
+        counter = carouselImages.length - counter;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
 
